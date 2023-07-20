e8a0379562504240fab35730aaf7a292460c88d6
boorapa007 <6340011007@psu.ac.th>
2023-07-21 00:21:16 +0700

    feat : new app

---
+++ lib/strawberry_pavlova.dart
@@ -0,0 +1,269 @@
+import 'package:flutter/material.dart';
+
+class strawberry_pavlova extends StatelessWidget {
+  const strawberry_pavlova({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    double Screen_width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Strawberry Pavlova"),
+      ),
+      body: 
+
+      Container(
+        child: 
+        Row(
+          children: [
+            Expanded(
+              flex: 1,
+              child:
+            Column(
+              children: [
+
+
+
+          Container(
+            child: Column(children: [
+              Container(
+                child: (Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      "Strawberry Pavlova",
+                      style: TextStyle(
+                        fontSize: 30,
+                      ),
+                    ),
+                  ],
+                )),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Container(
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          "Pavlova is a meringue-based",
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          "dessert named after the Russian",
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          "ballerine Anna Pavlova. ",
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          "Pavlova featues a crisp crust and",
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          "soft, light inside, topped with fruit",
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          "and whipped cream.",
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Container(
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Container(
+                      child: Row(
+                        children: [
+                          Text(
+                            " ★ ★ ★ ★ ★",
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      width: 40,
+                    ),
+                    Container(
+                      child: Row(
+                        children: [
+                          Text(
+                            "170 Reviews",
+                            style: TextStyle(
+                              fontSize: 20,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+          
+              SizedBox(
+                height: 20,
+              ),
+          
+              Container(
+                child: Container(
+                  child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Column(
+                        children: [
+                          Row(
+                            children: [
+                              Icon(
+            Icons.kitchen,
+            ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Text("PREP:"),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Text("25 min"),
+                            ],
+                          ),
+                        ],
+                      ),
+          
+                      SizedBox(
+                        width: 20,
+                      ),
+          
+          
+                      Container(
+                        child: Column(
+                        children: [
+                          Row(
+                            children: [
+                            Icon(
+            Icons.schedule,
+            ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Text("COOK:"),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Text("1 hr"),
+                            ],
+                          ),
+                        ],
+                      ),
+                      ),
+          
+                                      SizedBox(
+                        width: 20,
+                      ),
+          
+                      
+                      Container(
+                        child: Column(
+                        children: [
+                          Row(
+                            children: [
+                             Icon(
+            Icons.restaurant,
+            ),
+          
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Text("FEEDS:"),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Text("4-6"),
+                            ],
+                          ),
+                        ],
+                      ),
+                      )
+                    ],
+                  ),
+                ),
+                
+              ),
+              
+            ]),
+          ),
+                        ],
+            ),
+            ),
+
+            Expanded(
+              flex: 1,
+              child: 
+              Image(image: NetworkImage("https://hips.hearstapps.com/hmg-prod/images/delish-200824-ghost-cake-0001-landscape-ag-1598886563.jpg?crop=0.829xw:0.623xh;0.0881xw,0.0745xh&resize=1200:*" 
+              ),width:Screen_width,
+              fit: BoxFit.cover
+              )
+            ),
+          ],
+          
+        ),
+      ),
+
+    );
+  }
+}
